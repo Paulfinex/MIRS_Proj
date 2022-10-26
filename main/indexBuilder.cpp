72977ba6e05b85829c3abfb028938add34aa5f71
DgL1797 <digiacomoluca1797@gmail.com>
2022-10-26 17:56:28 +0200

    update scaletta

--- main/indexBuilder.cpp
+++ main/indexBuilder.cpp
@@ -6,12 +6,35 @@ using namespace std;
 int main(int argc, char const *argv[]){
   /**
    * @todo reading file line-by-line and foreach line{
-   *    tolowercase
    *    punctuation and extra characters removal
    *    space normalization(removing extra spaces and trim)
-   *    stopwords removal
+   *    tolowercase of the single tokens
+   *    stopwords removal by simply not inserting the token if it is in the stopwords
    *    stemming
+   *    saving of the token into vocabulary where we save for each termid the length of the posting list
+   *    saving the posting list data structure as a sorted skipping list for each token
    * } 
    */
+  try{
+    ifstream infile("../data/input/sample_dataframe.tsv");
+    while(!infile.eof()){
+      string docno;
+      string docbody;
+      getline(infile, docno, '\t');
+      getline(infile, docbody, '\n');
+      
+      /**
+       * @todo implementing document table:
+       *    docno to docid mapping
+       *    each docno must be mapped to a docid and a body length
+       *    body length must be calculated over the modified body to better assess the doc_score in ranking
+       */
+      
+    }
+  }catch(ifstream::failure& e){
+    cout << e.what() << endl;
+    exit(EXIT_FAILURE);
+  }
+  
   return 0;
 }
