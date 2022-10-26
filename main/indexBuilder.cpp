56b8e1b9a1cfa5ee822aa11660571a79cb7ec9f7
DgL1797 <digiacomoluca1797@gmail.com>
2022-10-26 16:58:54 +0200

    work in progress

--- main/indexBuilder.cpp
+++ main/indexBuilder.cpp
@@ -1,6 +1,35 @@
-#include "headers/olestem/stemming/english_stem.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <filesystem>
+#include <locale>
+#include <codecvt>
+#include "../headers/olestem/stemming/english_stem.h"
+using namespace std;
 
 int main(int argc, char const *argv[]){
+  filesystem::path infilePath("../data/input/sample_dataframe.tsv");
+  try{
+    wifstream infile(infilePath, ios::in);
+    while(!infile.eof()){
+      wstring docbody;
+      wstring docno;
+      getline(infile, docno, L'\t');
+      getline(infile, docbody, L'\n');
+      wstring_convert<std::codecvt<char16_t, wchar_t, mbstate_t>, char16_t> converter;
+      u16string _docno = converter.from_bytes(docno);
+      if(docno == L"5997279"){
+        wcout << docno << L">> " << docbody << L"\n";
+        system("pause");
+      }else{
+        wcout << docno << L">> " << docbody << L"\n";
+      }
+    }
+  }
+  catch(const wifstream::failure& e)
+  {
+    std::cerr << e.what() << '\n';
+  }
   
   return 0;
 }
