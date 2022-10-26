acb6ee2f5b87ecda08ab743ff797ffff828d377a
DgL1797 <digiacomoluca1797@gmail.com>
2022-10-26 17:30:58 +0200

    scaletta

--- main/indexBuilder.cpp
+++ main/indexBuilder.cpp
@@ -4,6 +4,14 @@
 using namespace std;
 
 int main(int argc, char const *argv[]){
-  
+  /**
+   * @todo reading file line-by-line and foreach line{
+   *    tolowercase
+   *    punctuation and extra characters removal
+   *    space normalization(removing extra spaces and trim)
+   *    stopwords removal
+   *    stemming
+   * } 
+   */
   return 0;
 }
